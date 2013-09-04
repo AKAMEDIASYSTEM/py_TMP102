dd9b42219f21c0eff5d437dcd92f76ea4ae80385
Noah Feehan <nfeehan@gmail.com>
2013-09-04 10:37:10 -0400

    getting test up and running

--- TMP102.py
+++ TMP102.py
@@ -14,7 +14,9 @@ class TMP102():
 
 	def getTemp(self):
 		msb = self.i2c.readU8(self.TMP102_ADDRESS)
+		print 'msb is ' msb
 		lsb = self.i2c.readU8(self.TMP102_ADDRESS)
+		print 'lsb is ' lsb
 		result = 0.0625*(((msb << 8 ) | lsb) >> 4)
 		return result
 		# uh, this might be wrong. Might need to 

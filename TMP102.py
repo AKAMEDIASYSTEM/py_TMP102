43e9d61b2ca907cc4c035e19e6a19c3c47e260d8
Noah Feehan <nfeehan@gmail.com>
2013-09-04 11:56:11 -0400

    getting test up and running

--- TMP102.py
+++ TMP102.py
@@ -5,7 +5,7 @@ from Adafruit_I2C import Adafruit_I2C
 import time
 
 class TMP102():
-    TMP102_ADDRESS = 0x91 # b1001000 addr pin pulled low
+    TMP102_ADDRESS = 0x48 # b1001000 addr pin pulled low
     # TMP102_ADDRESS = 0x49 # b1001001 addr pin pulled high
     # TMP102_ADDRESS = 0x4A b1001010 addr pin tied to SDA
     # TMP102_ADDRESS = 0x4B b1001011 addr pin tied to SCL
@@ -13,7 +13,7 @@ class TMP102():
     TMP102_READ_FROM_TEMP = 0x00 # write this to the pointer reg to init temp readings
     def __init__(self, *args, **kwargs):
         self.i2c = Adafruit_I2C(self.TMP102_ADDRESS)
-        self.i2c.write8(self.TMP102_WRITE_TO_POINTER, self.TMP102_READ_FROM_TEMP)
+        # self.i2c.write8(self.TMP102_WRITE_TO_POINTER, self.TMP102_READ_FROM_TEMP)
 
     def getTemp(self):
         rd = self.i2c.readU16(self.TMP102_ADDRESS)

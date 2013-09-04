7987a3bccf9e6c0a617808bbb98e900c022ec141
Noah Feehan <nfeehan@gmail.com>
2013-09-04 10:56:53 -0400

    getting test up and running

--- TMP102.py
+++ TMP102.py
@@ -7,15 +7,19 @@ import time
 class TMP102():
     TMP102_ADDRESS = 0x48 # b1001000 addr pin pulled low
     # TMP102_ADDRESS = 0x49 # b1001001 addr pin pulled high
-    # TMP102_ADDRESS = 0x4A b1001010 tie to SDA
-    # TMP102_ADDRESS = 0x4B b1001011 tie to SCL
+    # TMP102_ADDRESS = 0x4A b1001010 addr pin tied to SDA
+    # TMP102_ADDRESS = 0x4B b1001011 addr pin tied to SCL
+    TMP102_WRITE_TO_POINTER = 0x92
+    TMP102_READ_FROM_TEMP = 0x00
     def __init__(self, *args, **kwargs):
         self.i2c = Adafruit_I2C(self.TMP102_ADDRESS)
+        self.i2c.write8(self.TMP102_WRITE_TO_POINTER)
+        self.i2c.write8(self.TMP102_READ_FROM_TEMP)
 
     def getTemp(self):
-        msb = self.i2c.readU16(0x91)
+        msb = self.i2c.readU16(self.TMP102_ADDRESS)
         print 'msb is ',msb
-        msb = msb >> 4
+        # msb = msb >> 4
         # lsb = self.i2c.readU8(self.TMP102_ADDRESS)
         # print 'lsb is ',lsb
         # result = 0.0625*(((msb << 8 ) | lsb) >> 4)
